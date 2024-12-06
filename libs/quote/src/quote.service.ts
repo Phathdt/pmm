3e487d577c8c6c88b313e8568ccee54f47bd1945
Phathdt <phathdt379@gmail.com>
2024-12-06 17:46:27 +0700

    update pmm Receiving address

--- libs/quote/src/quote.service.ts
+++ libs/quote/src/quote.service.ts
@@ -105,7 +105,7 @@ export class QuoteService {
         toTokenPrice
       );
 
-      const pmmAddress = this.getPmmAddressByNetworkType(toToken);
+      const pmmAddress = this.getPmmAddressByNetworkType(fromToken);
 
       await this.sessionRepo.save(sessionId, {
         fromToken: dto.fromTokenId,
