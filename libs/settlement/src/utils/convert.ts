339d59364b387576c542723dae92db319aad67f9
Phathdt <phathdt379@gmail.com>
2025-02-24 12:35:30 +0700

    remove not using fucntion

--- libs/settlement/src/utils/convert.ts
+++ libs/settlement/src/utils/convert.ts
@@ -1,20 +1,7 @@
 import { ensureHexPrefix } from '@bitfi-mock-pmm/shared'
-import { Token } from '@petafixyz/market-maker-sdk'
+
 import { toUtf8Bytes, toUtf8String } from 'ethers'
 import { ethers } from 'ethers'
-
-export const decodeAddress = (value: string, token: Token) => {
-  switch (token.networkType.toUpperCase()) {
-    case 'EVM':
-      return ethers.getAddress(value)
-    case 'BTC':
-    case 'TBTC':
-    case 'SOLANA':
-      return ethers.toUtf8String(value)
-    default:
-      throw new Error(`Unsupported network: ${token.networkType}`)
-  }
-}
 
 export const l2Encode = (info: string) => {
   if (/^0x[0-9a-fA-F]*$/.test(info)) {
