0e70cf5431de62710fa2cc56c2f38afaf7abf3e8
Phathdt <phathdt379@gmail.com>
2025-07-25 16:13:17 +0700

    refactor: 🔄 update RPC URL handling in provider.ts (#21)

--- libs/shared/src/provider.ts
+++ libs/shared/src/provider.ts
@@ -1,20 +1,24 @@
 import { ethers } from 'ethers'
 
-import { BASE_MAINNET, ETHEREUM, ETHEREUM_SEPOLIA, SEPOLIA_BASE } from './constants'
+import { ETHEREUM, ETHEREUM_SEPOLIA } from './constants'
 
 export const getRpcUrlByNetworkId = (networkId: string): string => {
-  switch (networkId) {
-    case ETHEREUM:
-      return 'https://eth-mainnet.public.blastapi.io'
-    case ETHEREUM_SEPOLIA:
-      return 'https://eth-sepolia.public.blastapi.io'
-    case SEPOLIA_BASE:
-      return 'https://base-sepolia.public.blastapi.io'
-    case BASE_MAINNET:
-      return 'https://base-mainnet.public.blastapi.io'
-    default:
-      throw new Error(`Network ID ${networkId} not supported`)
+  const rpcUrl = (() => {
+    switch (networkId) {
+      case ETHEREUM:
+        return process.env['ETH_RPC_URL']
+      case ETHEREUM_SEPOLIA:
+        return process.env['ETH_SEPOLIA_RPC_URL']
+      default:
+        throw new Error(`Network ID ${networkId} not supported`)
+    }
+  })()
+
+  if (!rpcUrl) {
+    throw new Error(`RPC URL for network ID ${networkId} not found`)
   }
+
+  return rpcUrl
 }
 
 export const getProvider = (networkId: string) => {
